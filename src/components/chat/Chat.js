2feb9479cbb6e90b6aae3bf1a384742b060609c3
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-05-22 05:04:28 +0400

    removed the scrollbar from the sidebar-chats

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import "./chat.css"
+import SeearchIcon from '@mui/icons-material/Search'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { IconButton, Avatar } from '@mui/material';
 
 export default function Chat() {
   return (
-<div className='chat'>
-       <h3> I'm The Chat</h3>
-   </div>
+    <div className='chat'>
+      <div className='chat-header'>
+        < Avatar />
+
+        <div className='chat-header-info'>
+          <h3> Room name</h3>
+          <p> Last seen 12:34pm</p>
+        </div>
+
+        <div className='chat-header-right'>
+          < IconButton>
+            < SeearchIcon />
+          </IconButton>
+
+          <IconButton>
+            < MoreVertIcon />
+          </IconButton>
+          
+        </div>
+      </div>  
+    </div>
   );
 }
