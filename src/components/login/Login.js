ee958f134ea81c0c285c8f1c07d596b5f13b4ec6
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-07-19 00:01:06 +0400

    trying to fix the google detection problem

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
         <div className="header-logo">
           <img
             className="logo"
-            src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
             alt="whatsap logo"
           />
         </div>
